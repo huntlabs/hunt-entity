7ec2a4e5c61f6c001666e5dad70490c5a7a87e3b
viile <dakgzhu@foxmail.com>
2017-08-24 03:36:11 -0400

    add dbal
    Former-commit-id: a5c5eacb857546ac62e0fe06546aa58959c78e9e

--- source/entity/dialect/postgresqldialect.d
+++ source/entity/dialect/postgresqldialect.d
@@ -4,8 +4,8 @@ import entity;
 
 class PostgresqlDialect : Dialect
 {
-	string closeQuote() { return "\""; }
-	string openQuote()  { return "\""; }
+	string closeQuote() { return "'"; }
+	string openQuote()  { return "'"; }
     Variant fromSqlValue(FieldInfo info)
 	{
 		if(typeid(info.fieldType) == typeid(dBoolType)){
