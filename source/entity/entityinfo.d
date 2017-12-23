daf1f894a981cf6a34aec2ba4e4372ec86337a4e
viile <zhudj@putao.com>
2017-12-23 17:25:28 +0800

    compare func
    Former-commit-id: ced9b9b1d35aebbaf0eea10cda99f0928a57ef0a

---
+++ source/entity/entityinfo.d
@@ -0,0 +1,80 @@
+module entity.entityinfo;
+
+import entity;
+
+class EntityInfo
+{
+	string name;
+	string tableName;
+	FieldInfo[string] fields;
+	string[] fieldsColumn;
+	string primaryKey;
+
+    alias int function(Object,EntityInfo,EntityManager,EntitySession) PersistFunc;
+    alias int function(Object,EntityInfo,EntityManager,EntitySession) RemoveFunc;
+    alias int function(Object,EntityInfo,EntityManager,EntitySession) MergeFunc;
+    alias Object function(Object,EntityInfo,EntityManager,EntitySession) FindFunc;
+    alias string[] function(Object,Object) CompareFunc;
+    alias Object function(Object,Variant) SetPriKeyFunc;
+    alias Variant function(Object) ReadPriKeyValueFunc;
+
+    PersistFunc persistFunc;
+    RemoveFunc removeFunc;
+    MergeFunc mergeFunc;
+    FindFunc findFunc;
+	CompareFunc compareFunc;
+	SetPriKeyFunc setPriKeyFunc;
+	ReadPriKeyValueFunc readPriKeyValueFunc;
+
+	this(string name,string tableName,string primaryKey,FieldInfo[string] fields,
+     PersistFunc persist,RemoveFunc remove,MergeFunc merge,FindFunc find,CompareFunc compare,SetPriKeyFunc setPriKey,
+	 ReadPriKeyValueFunc readPriKeyValue)
+	{
+		this.name = name;
+		this.tableName = tableName;
+		this.primaryKey = primaryKey;
+		this.fields = fields;
+
+        this.persistFunc = persist;
+        this.removeFunc = remove;
+        this.mergeFunc = merge;
+        this.findFunc = find;
+		this.compareFunc = compare;
+		this.setPriKeyFunc = setPriKey;
+		this.readPriKeyValueFunc = readPriKeyValue;
+	}
+
+	string[] getAllFields()
+	{
+		if(!fieldsColumn.length){
+			foreach(k,v;fields){
+				fieldsColumn ~= k;
+			}
+		}
+		return fieldsColumn;
+	}
+
+	string getPrimaryKey()
+	{
+		return primaryKey;
+	}
+
+	T getPrimaryKeyValue(T)(Object obj)
+	{
+		return cast(T)*readPriKeyValueFunc(obj).peek!T;	
+	}
+
+	void setPrimaryKey(T)(Object obj,T value)
+	{
+		setPriKeyFunc(obj,Variant(value));
+	}
+
+    FieldInfo opDispatch(string name)() 
+    {
+        return fields.get(name,null);
+    }
+    FieldInfo opIndex(string name) 
+    {
+        return fields.get(name,null);
+    }
+}
---
+++ source/entity/entitymanager.d
@@ -0,0 +1,229 @@
+module entity.entitymanager;
+
+import entity;
+
+class EntityManager
+{
+	string name;
+	DatabaseConfig config;
+	Database db;
+	Dialect dialect;
+	EntityInfo[string] entityList;
+	EntityInfo[TypeInfo_Class] classMap;
+    SqlFactory sqlFactory;
+
+    bool logStatus = true;
+
+
+	this(string name,DatabaseConfig config,Database db,Dialect dialect,
+			EntityInfo[string] entityList,
+			EntityInfo[TypeInfo_Class] classMap)
+	{
+		this.name = name;
+		this.config = config;
+		this.dialect = dialect; 
+		this.db = db;
+		this.entityList = entityList;
+		this.classMap = classMap;
+        sqlFactory = new SqlFactory();
+	}
+    
+
+    EntitySession createEntityTransaction()
+    {
+        return new EntitySession(this);
+    }
+
+	SqlBuilder createSqlBuilder()
+    {
+        version(USE_MYSQL){
+            return sqlFactory.createMySqlBuilder();
+        }
+        version(USE_POSTGRESQL){
+            return sqlFactory.createPostgresqlSqlBuilder();
+        }
+        version(USE_SQLITE){
+            return sqlFactory.createSqliteBuilder();
+        }
+    }
+	
+	CriteriaBuilder createCriteriaBuilder(T)()
+	{
+		return new CriteriaBuilder(config,db,dialect,this,T.stringof,entityList,classMap);
+	}
+
+	int persist(Object obj)
+	{
+		auto info = findEntityForObject(obj);
+		auto session = createEntityTransaction();
+        int result = info.persistFunc(obj,info,this,session);
+        session.commit();
+        return result;
+	}
+
+	Object find(Object obj)
+	{
+		auto info = findEntityForObject(obj);
+		auto session = createEntityTransaction();
+		auto result = info.findFunc(obj,info,this,session);
+        session.commit();
+        return result;
+	}
+
+	Object find(T,F)(F value)
+	{
+		auto t = new T();
+		auto info = findEntityForObject(t);
+		info.setPriKeyFunc(t,Variant(value));
+		if(find(t) is null)
+			return null;
+		return cast(T)t;
+	}
+	
+	string[] compare(Object objold,Object objnew)
+	{
+		auto info = findEntityForObject(objold);
+		return info.compareFunc(objold,objnew);
+	}
+
+
+	int remove(Object obj)
+	{
+		auto info = findEntityForObject(obj);
+		auto session = createEntityTransaction();
+		int result = info.removeFunc(obj,info,this,session);
+        session.commit();
+        return result;
+	}
+	
+	int remove(T,F)(F value)
+	{
+		auto t = new T();
+		auto info = findEntityForObject(t);
+		info.setPriKeyFunc(t,Variant(value));
+		return remove(t);
+	}
+
+	int merge(Object obj)
+	{
+		auto info = findEntityForObject(obj);
+		auto session = createEntityTransaction();
+		int result = info.mergeFunc(obj,info,this,session);
+        session.commit();
+        return result;
+	}
+
+    int execute(SqlSyntax syntax)
+    {
+        entityLog(syntax.toString); 
+		return execute(syntax.toString);	
+    }
+	int execute(SqlBuilder builder)
+	{
+        entityLog(builder.build().toString); 
+		return execute(builder.build().toString);	
+	}
+	int execute(CriteriaBuilder builder)
+	{
+        entityLog(builder.toString);
+		return execute(builder.toString);	
+	}
+	int execute(string sql)
+	{
+        entityLog(sql);
+		return db.execute(sql);	
+	}
+    
+    int execute(T)(T t)
+    {
+        string str;
+        static if(is(T == SqlSyntax))
+            str = t.toString();
+        else static if(is(T == SqlBuilder))
+            str = t.build().toString();
+        else static if(is(T == CriteriaBuilder))
+            str = t.toString();
+        else
+            str = t;
+        return execute(str);
+    }
+    
+	T getResult(T,F)(F cb)
+	{
+        string sql;
+        static if(is(F == CriteriaBuilder))
+            sql = cb.toString;
+        else
+            sql = cb;
+        entityLog(sql);
+		auto stmt = db.prepare(sql);
+		auto res = stmt.query();
+        if(!res.empty()){
+            auto r = res.front();
+            auto result = new T();
+            auto entity = findEntityForObject(result);
+            foreach(field;entity.fields){
+                field.fieldValue = Variant(r[field.fieldName]);
+                field.write(result);
+            }
+            return result;
+        }
+		return null;
+	}
+
+	T[] getResultList(T,F)(F ct)
+	{
+		T[] result;
+        string sql;
+        static if (is(F == SqlBuilder))
+            sql = ct.build.toString;
+        else static if (is(F == string))
+            sql = ct;
+        else
+            sql = ct.toString;
+        entityLog(sql);
+		auto stmt = db.prepare(sql);
+		auto res = stmt.query();
+		foreach(r;res){
+			auto t = new T();
+			auto entity = findEntityForObject(t);
+			foreach(field;entity.fields){
+				field.fieldValue = Variant(r[field.fieldName]);
+				field.write(t);
+			}
+			result ~= t;
+		}
+		return result;
+	}
+
+	EntityInfo findEntityForObject(Object obj)
+	{
+		if(!(obj.classinfo in classMap))
+			throw new EntityException("Cannot find entity by class " ~ obj.classinfo.toString());	
+		return classMap[obj.classinfo];
+	}
+
+    void enableLog()
+    {
+        this.logStatus = true;
+    }
+
+    void disableLog()
+    {
+        this.logStatus = false;
+    }
+
+    void entityLog(T)(T value,string file = __FILE__, size_t line = __LINE__)
+    {
+        if(this.logStatus)log(file,":",line," ",value); 
+    }
+    
+    EntityInfo opDispatch(string name)() 
+    {
+        return entityList.get(name,null);
+    }
+    EntityInfo opIndex(string name) 
+    {
+        return entityList.get(name,null);
+    }
+}
