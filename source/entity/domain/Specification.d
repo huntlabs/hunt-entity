09f7246467eafc2d83c66076a38a0dfe416dcfae
Brian Zou <zoujiaqing@gmail.com>
2018-05-18 18:14:54 +0800

    Add some annotaion
    Former-commit-id: 2d6b122cee5cf643d83db59cce9140a2a12444dc

--- source/entity/domain/Specification.d
+++ source/entity/domain/Specification.d
@@ -1,10 +1,19 @@
-﻿module entity.domain.Specification;
+﻿/*
+ * Entity - Entity is an object-relational mapping tool for the D programming language. Referring to the design idea of JPA.
+ *
+ * Copyright (C) 2015-2018  Shanghai Putao Technology Co., Ltd
+ *
+ * Developer: HuntLabs.cn
+ *
+ * Licensed under the Apache-2.0 License.
+ *
+ */
+
+module entity.domain.Specification;
 
 import entity;
 
 interface Specification(T)
 {
-	Predicate toPredicate(Root!T root, CriteriaQuery!T criteriaQuery ,
-		CriteriaBuilder criteriaBuilder);
+	Predicate toPredicate(Root!T root, CriteriaQuery!T criteriaQuery, CriteriaBuilder criteriaBuilder);
 }
-
--- source/entity/domain/package.d
+++ source/entity/domain/package.d
@@ -1,4 +1,15 @@
-﻿module entity.domain;
+﻿/*
+ * Entity - Entity is an object-relational mapping tool for the D programming language. Referring to the design idea of JPA.
+ *
+ * Copyright (C) 2015-2018  Shanghai Putao Technology Co., Ltd
+ *
+ * Developer: HuntLabs.cn
+ *
+ * Licensed under the Apache-2.0 License.
+ *
+ */
+
+module entity.domain;
 
 public import entity.domain.Sort;
 public import entity.domain.Page;
