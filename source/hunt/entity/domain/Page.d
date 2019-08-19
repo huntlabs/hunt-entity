046e5c5ee134a022280e16b0ec02c0297f5bb5ac
hanfengyang <hanfengyang@putao.com>
2019-08-19 14:43:26 +0800

    add PageModel

--- source/hunt/entity/domain/Page.d
+++ source/hunt/entity/domain/Page.d
@@ -20,10 +20,20 @@ class Page(T)
 	Pageable	_pageable;
 	long		_total;
 
+	struct PageModel
+	{
+		int previous;
+		int current;
+		int next;
+		int[] pages;
+		int size;
+		long totalElements;
+		int totalPages;
+	}
 
-	this(T []content , 
-		Pageable pageable , 
-		long total)
+	this(T []content ,
+	Pageable pageable ,
+	long total)
 	{
 		_content = content;
 		_pageable = pageable;
@@ -34,8 +44,8 @@ class Page(T)
 	{
 		return _pageable.getPageNumber();
 	}
-	
-	int getSize()             
+
+	int getSize()
 	{
 		return _pageable.getPageSize();
 	}
@@ -44,32 +54,32 @@ class Page(T)
 	{
 		return cast(int)(_total / getSize() + (_total % getSize() == 0 ? 0 : 1));
 	}
-	
+
 	int getNumberOfElements()
 	{
 		return cast(int)_content.length;
 	}
-	
+
 	long getTotalElements()
 	{
 		return _total;
 	}
-	
+
 	bool hasPreviousPage()
 	{
 		return getNumber() > 0;
 	}
-	
+
 	bool isFirstPage()
 	{
 		return getNumber() == 0;
 	}
-	
+
 	bool hasNextPage()
 	{
 		return getNumber() < getTotalPages() - 1;
 	}
-	
+
 	bool isLastPage()
 	{
 		return getNumber() == getTotalPages() - 1;
@@ -79,14 +89,119 @@ class Page(T)
 	{
 		return _content;
 	}
-	
+
 	bool hasContent()
 	{
 		return _content.length > 0 ;
 	}
-	
+
 	Sort getSort()
 	{
 		return _pageable.getSort();
+	}
+
+	PageModel getModel(int num = 5)
+	{
+		if(num < 2)
+		{
+			num = 3;
+		}
+		if(num%2 == 0)
+		{
+			num++;
+		}
+		PageModel pm;
+
+		pm.current = this.getNumber() + 1;
+		if (this.hasNextPage())
+		{
+			pm.next = pm.current + 1;
+		}
+
+		if (this.hasPreviousPage())
+		{
+			pm.previous = pm.current - 1;
+		}
+
+		pm.size = this.getSize();
+		pm.totalPages = this.getTotalPages();
+		if(pm.totalPages < pm.current)
+		{
+			pm.current = pm.totalPages;
+		}
+		pm.totalElements = this.getTotalElements();
+
+		if(pm.totalPages <= (num*2+1))
+		{
+			for(int i = 1; i <= pm.totalPages; i++)
+			{
+				pm.pages ~= i;
+			}
+		}else{
+			int half = cast(int)(num/2);
+			int[] pages;
+			int[] firsts;
+			int[] lefts;
+			int[] rights;
+			int[] lasts;
+			if(pm.current - num >= half)
+			{
+				for(int i = 1; i <= half; i++)
+				{
+					firsts ~= i;
+				}
+				if(firsts.length > 0)
+				{
+					firsts ~= 0;
+				}
+			}
+
+			for(int i = pm.current - (num - cast(int)firsts.length); i < pm.current; i++)
+			{
+				if(i > 0)
+				{
+					lefts ~= i;
+				}
+			}
+
+			if(pm.current + num < pm.totalPages)
+			{
+				lasts ~= 0;
+				for(int i = (pm.totalPages - half) + 1; i <= pm.totalPages; i++)
+				{
+					lasts ~= i;
+				}
+			}
+
+			for(int i = pm.current + 1; i <= pm.current + (num - cast(int)lasts.length); i++)
+			{
+				if(i <= pm.totalPages)
+				{
+					rights ~= i;
+				}
+			}
+			if(pm.current <= half)
+			{
+				rights = [];
+				for(int i = pm.current + 1; i <= num; i++)
+				{
+					rights ~= i;
+				}
+			}
+			if(pm.totalPages - pm.current <= half)
+			{
+				lefts = [];
+				for(int i = pm.totalPages - num + 1; i < pm.current; i++)
+				{
+					lefts ~= i;
+				}
+			}
+			pm.pages ~= firsts;
+			pm.pages ~= lefts;
+			pm.pages ~= pm.current;
+			pm.pages ~= rights;
+			pm.pages ~= lasts;
+		}
+		return pm;
 	}
 }
